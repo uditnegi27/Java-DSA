1fd7e70293750f508ceaba773a6f3e28caf4e99b
udit <uditnegi27@gmail.com>
2022-07-25 23:27:15 +0530

    Insertion Sort

--- Sorting_Algorithm/InsertionSort.java
+++ Sorting_Algorithm/InsertionSort.java
@@ -2,8 +2,24 @@ package Sorting_Algorithm;
 
 public class InsertionSort {
 	
+	public static void swap(int arr[], int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	
 	public static void insertionSort(int arr[], int arrayLength) {
-		
+		for(int i=1; i<arrayLength; i++) {
+			int temp = arr[i];
+			int j = i-1;
+			
+			while(j>=0 && temp < arr[j]) {
+				arr[j+1] = arr[j];
+				j--;
+			}
+			
+			arr[j+1] = temp;
+		}
 	}
 
 	public static void main(String[] args) {
