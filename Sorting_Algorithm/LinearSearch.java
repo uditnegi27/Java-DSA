dda25b689a79d53cb58b40ea15dc49e5085ed2d6
udit <uditnegi27@gmail.com>
2022-07-25 18:45:37 +0530

    Linear Search

---
+++ Sorting_Algorithm/LinearSearch.java
@@ -0,0 +1,26 @@
+package Sorting_Algorithm;
+
+public class LinearSearch {
+	
+	public static boolean linearSearch(int arr[], int number) {
+		for(int i=0; i<arr.length; i++) {
+			if(arr[i] == number) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {1,2,3,4,5,6,7};
+	    int search  = 5;
+	    if(linearSearch(arr, search)){
+	        System.out.println("Element "+search+" is present in the array");
+	    }
+	    else{
+	    	System.out.println("Element "+search+" is not present in the array");
+	    }
+
+	}
+
+}
